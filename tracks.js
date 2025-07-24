5f0c1e620784ac9c9d1e8434e5602347ce68d309
JoshFleming <joshyfleming15@icloud.com>
2025-07-24 08:38:03 +0100

    Add horizontal tracks

--- tracks.js
+++ tracks.js
@@ -2,7 +2,28 @@
 class Tracks{
     constructor() {
         const t = this;
+        t.selectedTrack = 'tracks_straight.jpg';
         t.grid = document.getElementById('grid');
+        
+        const straightOption = document.getElementById('straight');
+        const horizOption = document.getElementById('horiz');
+        
+        straightOption.style["background-image"] = "url('tracks_straight.jpg')";
+        horizOption.style["background-image"] = "url('tracks_horiz.jpg')";
+        straightOption.style["outline"] = "3px solid blue";
+        
+        straightOption.addEventListener('click', () => {
+            t.selectedTrack = 'tracks_straight.jpg';
+            straightOption.style["outline"] = "3px solid blue";
+            horizOption.style["outline"] = "";
+        });
+        
+        horizOption.addEventListener('click', () => {
+            t.selectedTrack = 'tracks_horiz.jpg';
+            horizOption.style["outline"] = "3px solid blue";
+            straightOption.style["outline"] = "";
+        });
+        
         for (let i = 0; i < 64;i++){
             const cell = document.createElement('div');
             cell.className = 'cell';
@@ -11,10 +32,17 @@ class Tracks{
             cell.addEventListener('click', (e) => { t.cell_click(t, e.target); });
             t.grid.appendChild(cell);
         }
-    } //end constructor
+    } 
     
     cell_click(t, el) {
-        el.style["background-color"] = "blue";
+        if (el.style["background-image"]) {
+            el.style["background-image"] = "";
+        } else {
+            el.style["background-image"] = "url('" + t.selectedTrack + "')";
+            el.style["background-size"] = "cover";
+            el.style["background-position"] = "center";
+            el.style["background-repeat"] = "no-repeat";
+        }
     }
     
 }
